79e974c0d05e1d533b7d2f3f436612f0abe97f54
Mustafa Durmaz <mdmz_001@hotmail.com>
2022-02-01 20:17:46 +0300

    commit tree 663bdba4c44ce538effccd5c564e0fec6106c26b

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, removeTask }) {
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasks.map((task) => (
+        <Task
+          key={task.text}
+          text={task.text}
+          category={task.category}
+          removeTask={removeTask}
+        />
+      ))}
     </div>
   );
 }
